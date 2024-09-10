4441ae6d64e599fcf5448ab186d3345592674a04
guru-shirva <guruganesh.23cs044@sode-edu.in>
2024-09-10 15:23:43 +0530

    Add files via upload

---
+++ J1.java
@@ -0,0 +1,12 @@
+public class J3{
+	public static void main (String[] args){
+	String s1="GANESH";
+	String s2=s1.substring(5,6);
+	String s3=s1.substring(4,5);
+	String s4=s1.substring(3,4);
+	String s5=s1.substring(2,3);
+	String s6=s1.substring(1,2);
+	String s7=s1.substring(0,1);
+	System.out.println(s2+""+s3+""+s4+""+s5""+s6+""+s7+""):
+}
+}
